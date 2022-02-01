8b9f2a74c93500bdef42e42ff40ad87a3de2d063
timellinghaus <98760796+timellinghaus@users.noreply.github.com>
2022-02-01 20:22:12 +0100

    Added light animations

--- LightController/LightController.ino
+++ LightController/LightController.ino
@@ -4,9 +4,18 @@ const int YELLOW_LED = 48;
 const int BLUE_LED = 31;
 const int BUTTON = 45;
 
-enum light {
-  RED, GREEN, YELLOW, BLUE, OFF
-} lightColor;
+const int standardDelay = 50;
+const int longDelay = 125;
+
+enum Color {
+  RED, GREEN, YELLOW, BLUE
+} lightColor = BLUE;
+
+enum Mode {
+  STATIC, BLINKING, SOS, DISCO1, DISCO2, OFF
+} lightMode = OFF;
+
+int sosIt = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -19,31 +28,104 @@ void setup() {
 
 void loop() {
   int b = readButton();
-  if (b == 1) {
-    clickEvent();
-  } else if (b == 2) {
-    doubleClickEvent();
-  } else if (b == 3) {
-    holdEvent();
-  } else if (b == 4) {
-    longHoldEvent();
+  if (b == 1) { // single click
+    toggleLight();
+  } else if (lightMode != OFF && b == 2) { // double click
+    switchColor();
+  } else if (lightMode != OFF && b == 3) { // click + hold
+    switchLightMode();
+  }
+  updateLEDs();
+}
+
+void updateLEDs() {
+  int ledPin = getLEDPin(lightColor);
+
+  if (lightMode == STATIC) {
+    turnLightsOff();
+    digitalWrite(ledPin, HIGH);
+  } else if (lightMode == BLINKING) {
+    blink(ledPin, standardDelay);
+  } else if (lightMode == SOS) {
+    int sosPart = sosIt / 3;
+    if (sosIt == 9) {
+      sosIt = -1;
+      delay(longDelay);
+    } else if (sosPart % 2 == 0) {
+      blink(ledPin, standardDelay);
+    } else {
+      blink(ledPin, longDelay);
+    }
+    sosIt++;
+  } else if (lightMode == DISCO1) {
+    blink(ledPin, standardDelay);
+    switchColor();
+  } else if (lightMode == DISCO2) {
+    digitalWrite(ledPin, !digitalRead(ledPin));
+    delay(100);
+    switchColor();
+  } else {
+    turnLightsOff();
+    lightColor = BLUE;
   }
 }
 
-void clickEvent() {
-  digitalWrite(RED_LED, HIGH);
-  delay(100);
-  digitalWrite(RED_LED, LOW);
+void blink(int ledPin, int delayTime) {
+  digitalWrite(ledPin, HIGH);
+  delay(delayTime);
+  digitalWrite(ledPin, LOW);
+  delay(delayTime);
 }
 
-void doubleClickEvent() {
-  digitalWrite(BLUE_LED, HIGH);
-  delay(100);
+void turnLightsOff() {
+  digitalWrite(RED_LED, LOW);
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(YELLOW_LED, LOW);
   digitalWrite(BLUE_LED, LOW);
 }
 
-void holdEvent() {
-  digitalWrite(GREEN_LED, HIGH);
-  delay(100);
-  digitalWrite(GREEN_LED, LOW);
+void toggleLight() {
+  if (lightMode == OFF) {
+    lightMode = STATIC;
+  } else {
+    lightMode = OFF;
+  }
+}
+
+void switchColor() {
+  if (lightColor == RED) {
+    lightColor = GREEN;
+  } else if (lightColor == GREEN) {
+    lightColor = YELLOW;
+  } else if (lightColor == YELLOW) {
+    lightColor = BLUE;
+  } else {
+    lightColor = RED;
+  }
+}
+
+void switchLightMode() {
+  if (lightMode == STATIC) {
+    lightMode = BLINKING;
+  } else if (lightMode == BLINKING) {
+    lightMode = SOS;
+  } else if (lightMode == SOS) {
+    lightMode = DISCO1;
+  } else if (lightMode == DISCO1) {
+    lightMode = DISCO2;
+  } else {
+    lightMode = STATIC;
+  }
+}
+
+int getLEDPin(Color color) {
+  if (color == RED) {
+    return RED_LED;
+  } else if (color == GREEN) {
+    return GREEN_LED;
+  } else if (color == YELLOW) {
+    return YELLOW_LED;
+  } else {
+    return BLUE_LED;
+  }
 }
